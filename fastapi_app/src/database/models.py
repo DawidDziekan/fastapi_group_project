e87fcd0491b366bd1fafdc91e9443466608a0076
DawidDziekan <150060724+DawidDziekan@users.noreply.github.com>
2024-07-12 15:46:22 +0200

    base for project and first functions
    Added: (not tested yet)
    -autorization functions
    - creating user function
    -updated models
    - docker compose and Docerfile

---
+++ fastapi_app/src/database/models.py
@@ -0,0 +1,56 @@
+from sqlalchemy import Column, Integer, String, Boolean, func, Table, UniqueConstraint
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
+
+Base = declarative_base()
+
+photo_tag_table = Table(
+    'photo_tag', Base.metadata,
+    Column('photo_id', Integer, ForeignKey('photos.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    role = Column(String, default="user")
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False)
+
+class Photo(Base):
+    __tablename__ = 'photos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    url = Column(String)
+    description = Column(String)
+    tags = relationship("Tag", secondary=photo_tag_table)
+    created_at = Column(DateTime, server_default=func.now())
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True, index=True)
+    photo_id = Column(Integer, ForeignKey('photos.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    content = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+
+
+
+
+
